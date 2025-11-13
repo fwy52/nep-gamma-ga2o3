a8da34d1ec343de8040e60fd5e8d2d546b66fa79
fwy52 <2933745042@qq.com>
2025-11-13 11:07:26 +0800

    Add files via upload

---
+++ 5_spectral_thermal_properties/Ga3/hnemd/001/plot_kappa_shc.py
@@ -0,0 +1,111 @@
+import sys, subprocess
+import numpy as np
+from pylab import *
+from ase.io import read,write
+import importlib.metadata
+scipy_version = importlib.metadata.version('scipy')
+
+uc = read('POSCAR')
+struc = uc*(6,6,2)
+l = struc.cell.lengths()
+Lx, Ly, Lz = l[0], l[1], l[2] 
+struc.pbc = [True, True, True]
+struc
+write("model.xyz", struc)
+
+with open('run.in', 'r') as file:
+    for line in file:
+        line = line.strip()
+        if 'time_step' in line:
+            time_step = float(line.split()[1])
+        elif 'nvt' in line:
+            T = int(line.split()[2])
+        elif 'compute_shc' in line:
+            num_corr_points = int(line.split()[2])
+            max_corr_t = int(line.split()[1])*int(line.split()[2])/1000
+            dic = 'x' if int(line.split()[3]) ==0 else 'y' if int(line.split()[3]) ==1 else 'z'
+            freq_points = int(line.split()[4])
+            num_omega = int(line.split()[5])
+        elif 'compute_hnemd' in line:
+            hnemd_sample = int(line.split()[1])
+            Fex, Fey,Fez = line.split()[2], line.split()[3], line.split()[4]
+print('驱动力方向：', dic)
+
+run_time_out = subprocess.run("grep -A 10 'compute_hnemd' run.in | grep 'run' | head -n 1 | awk '{print $2}'", shell=True, capture_output=True, text=True)
+run_time = int(run_time_out.stdout.strip())
+kappa = np.loadtxt('kappa.out')
+t = np.arange(1, len(kappa) + 1) * 0.001 
+xlimit = int(run_time / 1000000)
+
+def running_ave(y: np.ndarray, x: np.ndarray) -> np.ndarray:
+    if scipy_version < '1.14':
+        from scipy.integrate import cumtrapz
+        return cumtrapz(y, x, initial=0) / x
+    else:
+        from scipy.integrate import cumulative_trapezoid
+        return cumulative_trapezoid(y, x, initial=0) / x 
+
+if dic == 'x':
+    ki_ra = running_ave(kappa[:,0],t)
+    ko_ra = running_ave(kappa[:,1],t)
+elif dic == 'y':
+    ki_ra = running_ave(kappa[:,2],t)
+    ko_ra = running_ave(kappa[:,3],t)
+else:
+    kz_ra = running_ave(kappa[:,4],t)
+
+
+
+shc = np.loadtxt('shc.out')
+V = l[0]*l[1]*l[2]
+Vvcf = shc[:(2*num_corr_points-1), :]
+shc_t, shc_Ki, shc_Ko = Vvcf[:, 0], Vvcf[:, 1], Vvcf[:, 2]
+shc_nu = shc[-freq_points:, 0]/(2*pi)
+Fe = Fex if dic == 'x' else Fey if dic == 'y' else Fez
+shc_kwi = shc[-freq_points:, 1] * 1602.17662 / (float(Fe) * T * V) #convert = 1602.17662
+shc_kwo = shc[-freq_points:, 2] * 1602.17662 / (float(Fe) * T * V)
+shc_kw = shc_kwi + shc_kwo
+shc_K = shc_Ki + shc_Ko
+
+Gc = np.load('Gc.npy')
+lambda_i =np.abs(shc_kw/Gc)
+length = np.logspace(1,6,100)
+k_L = np.zeros_like(length)
+numpy_version = importlib.metadata.version('numpy')
+for i, el in enumerate(length):
+    if numpy_version < '2':
+        k_L[i] = np.trapz(shc_kw/(1+lambda_i/el), shc_nu)
+    else:
+        k_L[i] = np.trapezoid(shc_kw/(1+lambda_i/el), shc_nu)
+
+figure(figsize=(10,5))
+
+
+
+
+subplot(1,2,1)
+plot(shc_nu, shc_kw,linewidth=3)
+xlim([0, 22])
+gca().set_xticks(linspace(0,22,7))
+ylim([0, 1.5])
+gca().set_yticks(linspace(0,1.5,6))
+ylabel(r'$\kappa$($\omega$) (W/m/K/THz)')
+xlabel(r'$\nu$ (THz)')
+title('(b)')
+
+
+
+subplot(1,2,2)
+plot(shc_nu, lambda_i,linewidth=3)
+xlim([0, 22])
+gca().set_xticks(linspace(0,22,7))
+ylim([0, 1000])
+gca().set_yticks(linspace(0,1000,6))
+ylabel(r'$\lambda$($\omega$) (nm)')
+xlabel(r'$\nu$ (THz)')
+title('(c)')
+
+
+
+
+savefig('shc.png', dpi=300, bbox_inches='tight')
